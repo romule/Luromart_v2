143efd4ce045d18dc3e41bb3f06016857b41edc8
Elijah Romashchenko <elijahro93@gmail.com>
2025-09-04 22:37:27 -0300

    add Pretier

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,2 +1,26 @@
-const AboutMe = () => <h1>About Me</h1>;
+const AboutMe = () => (
+  <section>
+    <h2>Shortly about me</h2>
+    <p>
+      Good morning, everyone. My name is Liudmyla Romashchenko, and I am Ukrainian. 
+      I am a painter. My artistic talent was noticed by my father, with whom 
+      I had a very close relationship. Thanks to him, I completed an art school, but unfortunately, 
+      my father suddenly passed away, and everything that reminded me of him caused a lot of pain, 
+      including painting. That's why I stopped painting. It continued until one day when 
+      I found myself in a church and realized that I have a Father who will never die 
+      – that is the Lord God. From that moment on, everything started falling into place.
+       I met my husband, we had a son, and I returned to painting. Initially, 
+       I painted on the occasions of celebrations for my friends. As a result, my paintings, 
+       along with their owners, ended up in the United States, Canada, France, England, Italy, 
+       Russia, Moldova, Ukraine, and now there are many in Poland. After the events of Maidan 
+       in Ukraine, I actively participated in various exhibitions to support the families of the 
+       Maidan victims and those affected by the war in the east. I also participated in the
+        anniversary celebrations of the Brodmann brand (piano manufacturer). Additionally, 
+        I took part in various creative concerts, exhibitions, and readings. In 2016, 
+        I participated in an exhibition-auction in Poland to support Christian 
+        families in Muslim countries. I also illustrated children's books. I had a multifaceted collaboration with Ukrainian writer Anna Grygorowich, with the most successful work being the illustrations for the fairy tale about the filly Camellia. We even staged a puppet show based on that story. In 2016, I participated in an illustration competition for the fairy tale "The Daughter of the Storyteller" organized by the Biedronka store in Poland. I had my exhibitions in Mińsk Mazowiecki as part of the "Painters of the Mińsk Land" project from September 7 to 30, 2018.
+    </p>
+  </section>
+);
+
 export default AboutMe;
